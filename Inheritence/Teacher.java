e1e836717366893c89f2ac92795d81efe84c4944
Sourav Dev <130246603+souravdev33@users.noreply.github.com>
2024-05-10 10:08:19 +0600

    Add files via upload

---
+++ Inheritence/Teacher.java
@@ -0,0 +1,13 @@
+
+package inheritence;
+
+
+public class Teacher extends Person {
+    
+     String qualification;
+
+    void display12(){
+        display1();
+        System.out.println("The qualification is :"+qualification);
+    }
+}
---
+++ Inheritence/Test.java
@@ -0,0 +1,15 @@
+
+package inheritence;
+
+
+public class Test {
+    
+    public static void main(String[] args) {
+        Teacher t1=new Teacher();
+        t1.name="Sourav";
+        t1.gender="male";
+        t1.age=22;
+
+        t1.display12();
+}
+}
